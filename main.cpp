635593ef020abe71b4f38e3ea83cbca76a7129c3
Erick <erickgtorelli@gmail.com>
2015-09-27 18:25:28 -0600

    Arreglado pequeño problema de referencias en el main

--- main.cpp
+++ main.cpp
@@ -16,19 +16,19 @@ using namespace std;
  */
 int main(int argc, char** argv) {
     
-    int tiempoPorSimulacion;
-    int tiempoToken;
-    int numeroDeVeces;
-    int modoLento = 0;
+	int tiempoPorSimulacion;
+	int tiempoToken;
+	int numeroDeVeces;
+	int modoLento = 0;
 
     printf("¿Cuantas veces quiere correr la simulación? \n");
-    scanf("%i",numeroDeVeces);
+	scanf("%d",&numeroDeVeces);
     printf("¿Cuanto tiempo desea por simulación? \n");
-    scanf("%i",tiempoPorSimulacion);
+	scanf("%d",&tiempoPorSimulacion);
     printf("¿Cuanto es el tiempo de token por computadpra? \n");
-    scanf("%i",tiempoToken);
+	scanf("%d",&tiempoToken);
     printf("¿Desea activar el modo lento? 1 para Si \n");
-    scanf("%i",modoLento);
+	scanf("%d",&modoLento);
 
 
 
